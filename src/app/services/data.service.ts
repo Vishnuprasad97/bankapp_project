71f1744b2ef619ed82d131c6cb2acc96e2357ecf
Vishnuprasad97 <vishnuprasad9391@gmail.com>
2023-02-28 12:58:35 +0530

    transaction page

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -4,12 +4,13 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class DataService {
-  currentuser:any
+  currentuser: any
+  currentacno:any
   userDetails: any = {
-    1000: { acno: 1000, username: 'amal', password: 123, balance: 0 },
-    1001: { acno: 1001, username: 'anu', password: 123, balance: 0 },
-    1002: { acno: 1002, username: 'anoop', password: 123, balance: 0 },
-    1003: { acno: 1003, username: 'anil', password: 123, balance: 0 },
+    1000: { acno: 1000, username: 'amal', password: 123, balance: 0,transaction:[]},
+    1001: { acno: 1001, username: 'anu', password: 123, balance: 0,transaction:[] },
+    1002: { acno: 1002, username: 'anoop', password: 123, balance: 0,transaction:[] },
+    1003: { acno: 1003, username: 'anil', password: 123, balance: 0 ,transaction:[]},
   };
 
   constructor() {}
@@ -19,7 +20,7 @@ export class DataService {
     if (acno in userDetails) {
       return false;
     } else {
-      userDetails[acno] = { acno, username, password, balance: 0 };
+      userDetails[acno] = { acno, username, password, balance: 0 ,transaction:[]};
       return true;
     }
   }
@@ -28,7 +29,9 @@ export class DataService {
     this.currentuser=userDetails[acno]['username']
     if (acno in userDetails) {
       if (password == userDetails[acno]['password']) {
+        this.currentacno=acno
         return true;
+
       } else {
         alert('Incorrect Password');
         return false;
@@ -44,6 +47,7 @@ export class DataService {
     if (acno in userDetails) {
       if (password == userDetails[acno]['password']) {
         userDetails[acno]['balance'] += amnt;
+        userDetails[acno]['transaction'].push({type:'CREDIT',amnt})
         return userDetails[acno]['balance'];
       } else {
         alert('Incorrect Password');
@@ -64,6 +68,7 @@ export class DataService {
           return false;
         } else {
           userDetails[acno]['balance'] -= amnt;
+          userDetails[acno]['transaction'].push({type:'DEBIT',amnt})
           return userDetails[acno]['balance'];
         }
       } else {
@@ -74,5 +79,9 @@ export class DataService {
       alert('User Does not exist');
       return false;
     }
+  }
+  getTransaction(acno: any) {
+    return this.userDetails[acno]['transaction']
+    
   }
 }
---
+++ src/app/transaction/transaction.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { DataService } from '../services/data.service';
+
+@Component({
+  selector: 'app-transaction',
+  templateUrl: './transaction.component.html',
+  styleUrls: ['./transaction.component.css'],
+})
+export class TransactionComponent {
+  useracno: any;
+  transaction: any;
+  constructor(private ds: DataService) {
+    this.useracno = this.ds.currentacno;
+    this.transaction = this.ds.getTransaction(this.useracno);
+    console.log(this.transaction);
+  }
+}
